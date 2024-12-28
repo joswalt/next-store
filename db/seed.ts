90096628ba2cb9bf60a6d14d8137dcf548c1e117
John Oswalt <joswalt@gmail.com>
2024-12-28 00:04:49 -0600

    Seed example user data

--- db/seed.ts
+++ db/seed.ts
@@ -4,12 +4,21 @@ import sampleData from "./sample-data";
 async function main() {
   const prisma = new PrismaClient();
 
-  // Delete all existing products
+  // Delete all from existing tables
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
   // Create new products
   await prisma.product.createMany({
     data: sampleData.products,
+  });
+
+  // Create new users
+  await prisma.user.createMany({
+    data: sampleData.users,
   });
 
   console.log(`Database has been seeded. 🌱`);
