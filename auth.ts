5c06002744a248dffc61eb29e3fc5713053fe66b
John Oswalt <joswalt@gmail.com>
2025-01-01 20:31:24 -0600

    Added jwt method

--- auth.ts
+++ auth.ts
@@ -1,11 +1,11 @@
 import NextAuth from "next-auth";
+import type { NextAuthConfig } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
-import type { NextAuthConfig } from "next-auth";
 
-export const config = {
+export const config: NextAuthConfig = {
   pages: {
     signIn: "/sign-in",
     error: "/sign-in",
@@ -54,6 +54,8 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       // Set the user ID from the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
       // If there is an update, set the user name
       if (trigger === "update") {
@@ -61,6 +63,22 @@ export const config = {
       }
 
       return session;
+    },
+    async jwt({ token, user }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role;
+        // If user has no name then use the email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
